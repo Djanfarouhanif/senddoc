b823ab94d86b4d75fa4e36ed962317f4f28303b6
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-06-29 17:09:41 +0000

    create doc models

--- core/models.py
+++ core/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+import uuid
+
+class Doc(models.Model):
+    id = models.CharField (max_length=500, default=uuid.uuid4, primary_key=True)
+    name = models.CharField(max_length=500)
+    semestre = models.IntegerField()
+    
+
+
+    def __str__(self):
+        return self.name
--- core/views.py
+++ core/views.py
@@ -5,6 +5,6 @@ from django.shortcuts import render
 
 def index(request):
 
-
+    
     return render(request, "index.html")
 
