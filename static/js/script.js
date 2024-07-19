cd8f4e351f66253e56ffcea4b4540c67c256e51d
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-19 11:29:06 +0000

    review code

--- static/js/script.js
+++ static/js/script.js
@@ -1,21 +1,28 @@
 const button = document.querySelector('#searchButton');
 const loader = document.querySelector(".loader");
 const table = document.querySelector('table');
+let isRunning = false;
+
 
 button.addEventListener('click', (event)=>{
     event.preventDefault();
-           loader.style.display = 'block';
+    
+    if(!isRunning){
+        isRunning = true;
+        loader.style.display = 'block';
+        table.style.display = 'none';
+
         setTimeout(()=>{
-            // loader.style.display = 'block';
-           $('#searchForm').submit();
-        }, 3000
-               
-        )
+            
+            isRunning = false;
+            loader.style.display = 'none';
+            table.style.display = 'block';
+        },3000);
+    }
+           
     
    
 })
-
-
 
 
     $(document).ready(function(){
