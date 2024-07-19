ca5eb3bf54bb6d1af006e3a6c74668aadeef3ca9
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-07-19 14:19:26 +0000

    code review

--- static/js/script.js
+++ static/js/script.js
@@ -1,64 +1,59 @@
 const button = document.querySelector('#searchButton');
 const loader = document.querySelector(".loader");
-const table = document.querySelector('table');
 let isRunning = false;
 
 
 button.addEventListener('click', (event)=>{
     event.preventDefault();
-    
+  
     if(!isRunning){
         isRunning = true;
         loader.style.display = 'block';
-        table.style.display = 'none';
+      
 
         setTimeout(()=>{
             
             isRunning = false;
             loader.style.display = 'none';
-            table.style.display = 'block';
-        },3000);
+        
+        },1000);
     }
            
     
-   
 })
 
 
-    $(document).ready(function(){
-        $('#searchForm').on('submit', function(event){
-            event.preventDefault();
-    
-            $.ajax({
-                type: 'POST',
-                url: "http://127.0.0.1:8000/",
-                data: $(this).serialize(),
-                success: function(response){
-                    $('#message').text('');
-                    $('#results').html('');
-                    if (response.message){
-                        $("#message").text(response.message);
-    
-                    }else {
-                        let table = '<table><caption><span>Licence </span> en Science de la vie et de la terre</caption><thead><tr><td>ID</td><td>UE</td><td>Semestre</td><td>Crédit</td><td>Télécharger</td></tr></thead><tbody>';
-                        $.each(response.results, function(index, result){
-                            table += '<tr><td class="td-1"><span>1</span></td><td class="td-2">' + result.ue + '</td><td class="td-3">Semestre ' + result.semestre + '</td><td class="td-4">' + result.credit + '</td><td class="td-5"><a href="#">Télécharger</a></td></tr>';
-                        });
-                        table += '</tbody></table>';
-                        $('#results').html(table);
-                    }
-                },
-                error: function(response){
-                    console.log("error",response)
-                },
-                complete: function(){
-                    loader.style.display = 'none';
-                    
+$(document).ready(function(){
+    $('#searchForm').on('submit', function(event){
+        event.preventDefault();
+
+        $.ajax({
+            type: 'POST',
+            url: "http://127.0.0.1:8000/",
+            data: $(this).serialize(),
+            success: function(response){
+                $('#message').text('');
+                $('#results').html('');
+                if (response.message){
+                    $("#message").text(response.message);
+
+                }else {
+                    let table = '<table><caption><span>Licence </span> en Science de la vie et de la terre</caption><thead><tr><td>ID</td><td>UE</td><td>Semestre</td><td>Crédit</td><td>Télécharger</td></tr></thead><tbody>';
+                    $.each(response.results, function(index, result){
+                        table += '<tr><td class="td-1"><span>1</span></td><td class="td-2">' + result.ue + '</td><td class="td-3">Semestre ' + result.semestre + '</td><td class="td-4">' + result.credit + '</td><td class="td-5"><a href="#">Télécharger</a></td></tr>';
+                    });
+                    table += '</tbody></table>';
+                    $('#results').html(table);
                 }
-            });
+            },
+            error: function(response){
+                console.log("error",response)
+            },
+            complete: function(){
+                loader.style.display = 'none';
+                
+            }
         });
     });
-
-
-
+});
 
