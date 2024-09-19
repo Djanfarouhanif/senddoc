348f22b53cb19fe4e94cff3d10e766053c87fc01
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-19 16:22:35 +0000

    create new table for registre user

--- core/models.py
+++ core/models.py
@@ -1,8 +1,20 @@
 from django.db import models
-
+from django.auth.models import User
 # Create your models here.
 import uuid
+user = User()
+#Creation studend table
+class Student(models.Model):
+    id = models.CharField(max_length=500, default=uuid.uuid4, primary_key = True)
+    user = models.ForeignKey(user, on_delete= models.CASCADE)
+    departemment = models.CharField(max_length=100)
 
+    def __str__(self):
+        return self.user.username
+#cree une class pour la creation des groupes de travaille
+
+
+    
 class Faculte(models.Model):
     id = models.CharField (max_length=500, default=uuid.uuid4, primary_key=True)
     faculte = models.CharField(max_length=500, default="")
