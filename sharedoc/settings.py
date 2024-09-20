234bec99a1406c6ebaa7370126a0a62695f7a4ca
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-20 20:13:38 +0000

    create serializer

--- sharedoc/settings.py
+++ sharedoc/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'core',
     'usercompte',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
---
+++ usercompte/serializer.py
@@ -0,0 +1,45 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from django.contrib.auth.password_validation import validate_password
+
+from django.contrib.auth import authenticate
+
+#Create user
+class SignupSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
+    password_confirm = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User 
+        fields = ('username', 'email', 'password', 'password_confirm')
+
+    def validate(self, attrs):
+        if attrs['password'] != attrs['password_confirm']:
+            raise serializers.validationError({'password': "Les mots de passe ne correspond pas "})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = User.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+        )
+        user.set_password(validated_data['password']) #Encrypt the password
+        user.save()
+
+        return user
+
+class SigninSerializer(serializers.Serializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(required=True, write_only =True)
+
+    def validate(self, attrs):
+        username = attrs.get('username')
+        password = attrs.get('password')
+
+        user = authenticate(username=username, password=password)
+        if not user:
+            raise serializers.validationError({"detail": "identifiants incorrects"})
+
+        attrs['user'] = user
+        return attrs
