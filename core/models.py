774384f692342ea264b989449770f2ff76b85a71
DJANFAROU <hanifdjanfarou8@gmail.com>
2024-09-22 16:50:56 +0000

    review code

--- core/models.py
+++ core/models.py
@@ -3,12 +3,10 @@ from django.contrib.auth.models import User
 # Create your models here.
 import uuid
 
-#Creation studend table
+#Creation studend table  
 
-#cree une class pour la creation des groupes de travaille
+# #cree une class pour la creation des groupes de travaille
 
-
-    
 class Faculte(models.Model):
     id = models.CharField (max_length=500, default=uuid.uuid4, primary_key=True)
     faculte = models.CharField(max_length=500, default="")
